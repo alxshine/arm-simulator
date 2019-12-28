f8fb2fc5330984ab411893567baf385455fce999
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-12-28 20:31:44 +0100

    Added prototype for STM and LDM

--- inc/cpu.hpp
+++ inc/cpu.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "type_definitions.hpp"
 
+#include <vector>
+
 namespace ARMSimulator {
 
 class Cpu {
@@ -14,26 +16,39 @@ class Cpu {
   void ADC(Register rd, Register r1, RightHandOperand op2,
            BarrelShifterConfig shiftConfig = {ShiftType::LogicalLeft, 0},
            bool setFlags = false);
+
   void ADD(Register rd, Register r1, RightHandOperand op2,
            BarrelShifterConfig shiftConfig = {ShiftType::LogicalLeft, 0},
            bool setFlags = false);
+
   void AND(Register rd, Register r1, RightHandOperand op2,
            BarrelShifterConfig shiftConfig = {ShiftType::LogicalLeft, 0},
            bool setFlags = false);
+
   void B(int address);
+
   void BIC(Register rd, Register r1, RightHandOperand op2,
            BarrelShifterConfig shiftConfig = {ShiftType::LogicalLeft, 0},
            bool setFlags = false);
+
   void BL(int address);
+
   void BX(RightHandOperand address);
+
   void CMN(Register r1, RightHandOperand op2,
            BarrelShifterConfig shiftConfig = {ShiftType::LogicalLeft, 0});
+
   void CMP(Register r1, RightHandOperand op2,
            BarrelShifterConfig shiftConfig = {ShiftType::LogicalLeft, 0});
+
   void EOR(Register rd, Register r1, RightHandOperand op2,
            BarrelShifterConfig shiftConfig = {ShiftType::LogicalLeft, 0},
            bool setFlags = false);
-  // TODO: LDM
+
+  void LDM(Register baseRegister, bool addressWriteBack, bool forceUserMode,
+           OffsetDirection offsetDirection, IndexingMethod indexingMethod,
+           std::vector<Register> registerList);
+
   void LDR(Register rd, Register baseRegister, RightHandOperand offset,
            bool addressWriteBack, TransferQuantity transferQuantity,
            OffsetDirection offsetDirection, IndexingMethod indexingMethod,
@@ -43,43 +58,60 @@ class Cpu {
            BarrelShifterConfig BarrelShifterConfig = {ShiftType::LogicalLeft,
                                                       0},
            bool setFlags = false);
+
   void MRS(Register rd);
+
   void MSR(Register r1);
+
   // TODO: MUL, MLA
   void MVN(Register rd, RightHandOperand op2,
            BarrelShifterConfig BarrelShifterConfig = {ShiftType::LogicalLeft,
                                                       0},
            bool setFlags = false);
+
   void ORR(Register rd, Register r1, RightHandOperand op2,
            BarrelShifterConfig BarrelShifterConfig = {ShiftType::LogicalLeft,
                                                       0},
            bool setFlags = false);
+
   void RSB(Register rd, Register r1, RightHandOperand op2,
            BarrelShifterConfig BarrelShifterConfig = {ShiftType::LogicalLeft,
                                                       0},
            bool setFlags = false);
+
   void RSC(Register rd, Register r1, RightHandOperand op2,
            BarrelShifterConfig BarrelShifterConfig = {ShiftType::LogicalLeft,
                                                       0},
            bool setFlags = false);
+
   void SBC(Register rd, Register r1, RightHandOperand op2,
            BarrelShifterConfig BarrelShifterConfig = {ShiftType::LogicalLeft,
                                                       0},
            bool setFlags = false);
-  // TODO: STM
+
+  void STM(Register rd, Register baseRegister, RightHandOperand offset,
+           bool addressWriteBack, TransferQuantity transferQuantity,
+           OffsetDirection offsetDirection, IndexingMethod indexingMethod,
+           BarrelShifterConfig shiftConfig = {ShiftType::LogicalLeft, 0});
+
   void STR(Register rd, Register baseRegister, RightHandOperand offset,
            bool addressWriteBack, TransferQuantity transferQuantity,
            OffsetDirection offsetDirection, IndexingMethod indexingMethod,
            BarrelShifterConfig shiftConfig = {ShiftType::LogicalLeft, 0});
+
   void SUB(Register rd, Register r1, RightHandOperand op2,
            BarrelShifterConfig BarrelShifterConfig = {ShiftType::LogicalLeft,
                                                       0},
            bool setFlags = false);
+
   void SWI();
+
   void SWP(Register destinationRegister, Register sourceRegister,
            Register baseRegister);
+
   void TEQ(Register r1, RightHandOperand op2,
            BarrelShifterConfig shiftConfig = {ShiftType::LogicalLeft, 0});
+
   void TST(Register r1, RightHandOperand op2,
            BarrelShifterConfig shiftConfig = {ShiftType::LogicalLeft, 0});
 
